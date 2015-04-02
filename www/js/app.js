7c020661871fcc6147e4ee3dd29d56525a1013cc
Enamul Haque <ehaque95@gmail.com>
2015-04-01 23:43:15 -0700

    Update app.js

--- www/js/app.js
+++ www/js/app.js
@@ -7,6 +7,7 @@ angular.module('starter', ['ionic' ,'starter.controllers','starter.directives','
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
+  	alert("app Controller");
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
     if (window.cordova && window.cordova.plugins.Keyboard) {
